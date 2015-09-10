5b82df18e45b52ccd28d4c11382a87e1ff34b68b
givemehtml5 <givemehtml5@gmail.com>
2015-09-10 16:37:56 +0200

    fillstyle update

--- game_v0.0.js
+++ game_v0.0.js
@@ -1,6 +1,7 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
-ctx.fillText("Hello World", canvas.width/2, canvas.height/2);
 ctx.fillStyle="#CEECF5";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
+ctx.fillStyle="#000000";
+ctx.fillText("Hello World", canvas.width/2, canvas.height/2);
 
